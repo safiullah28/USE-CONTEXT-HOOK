da10684e9fd3bcb5adb13c9c72ecebe033a7a830
safiullah28 <safiullah424@gmail.com>
2024-03-13 00:16:19 +0500

    Use Context Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useContext } from "react";
+import ChildA from "./ChildA";
+
+import Skills from "./skills.json";
+
+//Create, Provide , context = useContext(contextValue);
+let data1 = createContext();
+let data2 = createContext();
 
 function App() {
+  const name = "Safiullah";
+  const age = 21;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <data1.Provider value={name}>
+        <data2.Provider value={age}>
+          <ChildA />
+        </data2.Provider>
+      </data1.Provider>
+      <div>
+        {iconsData.map((skill) => {
+          return <div>
+            <img src={skill.icon} alt={skill.title} />
+            <h3>{skill.title}</h3>
+          </div>;
+        })}
+      </div>
     </div>
   );
 }
 
 export default App;
+export { data1, data2 };
